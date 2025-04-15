6b8a70952e42da270fceb38f2c2f9404fd60ea7f
bren112 <brenomarinho1610@gmail.com>
2025-04-15 11:50:29 -0300

    sever

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Isso permite que o servidor aceite conexões de qualquer IP
+    port: 3000,        // Você pode escolher outra porta, se necessário
+    open: true,        // Isso abre o navegador automaticamente ao rodar o servidor
+  },
 })
